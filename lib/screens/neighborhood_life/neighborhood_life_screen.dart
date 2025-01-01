fc647c914cad4a170eb01e06c58e1fec8a87915f
moohyun <angus1208@naver.com>
2025-01-01 22:12:11 +0900

    [feat] 동네 생활 화면 구현 완성

--- lib/screens/neighborhood_life/neighborhood_life_screen.dart
+++ lib/screens/neighborhood_life/neighborhood_life_screen.dart
@@ -1,12 +1,40 @@
+import 'package:carrot/models/neighborhood_life.dart';
+import 'package:carrot/screens/components/appbar_prefreed_size.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NeighborhoodLifeScreen extends StatelessWidget {
+import 'components/life_body.dart';
+import 'components/life_header.dart';
 
+class NeighborhoodLifeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("NeighborhoodLife Screen build");
     return Scaffold(
-      body: Center(child: Text("NeighborhoodLifeScreen")),
+      backgroundColor: Colors.grey[100],
+      appBar: AppBar(
+        title: Text('동네생활'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.search)),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(CupertinoIcons.plus_rectangle_on_rectangle)),
+          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bell)),
+        ],
+        bottom: appBarBottomLine(),
+      ),
+      body: ListView(
+        children: [
+          LifeHeader(),
+          ...List.generate(
+            neighborhoodLifeList.length,
+            (index) => Padding(
+              padding: const EdgeInsets.only(bottom: 12.0),
+              child: LifeBody(neighborhoodLife: neighborhoodLifeList[index]),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
