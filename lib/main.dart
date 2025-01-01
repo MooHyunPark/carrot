6b987b8c41f51498da893d1073f50f1cbeebdedc
moohyun <angus1208@naver.com>
2025-01-01 20:10:14 +0900

    [feat] 홈 화면 작성 완료

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class CarrotMarketUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'carrot',
       debugShowCheckedModeBanner: false,
       home: MainScreens(),
       theme: theme(),
